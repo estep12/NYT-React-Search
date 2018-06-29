1c3e259e807ef0459d4778062c3d41edec5f1e78
Erik Stephenson <33075989+estep12@users.noreply.github.com>
2018-06-28 22:20:01 -0500

    Getting connected to mongoDB

---
+++ server.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+// const routes = require("./routes");
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+// Require all models
+var db = require("./models");
+
+// Define middleware here
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+// Serve up static assets (usually on heroku)
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+}
+
+// Add routes, both API and view
+// app.use(routes);
+
+ 
+// If deployed, use the deployed database. Otherwise use the local newsapp database
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/nytreact";
+
+// Set mongoose to leverage built in JavaScript ES6 Promises
+// Connect to the Mongo DB
+mongoose.Promise = Promise;
+mongoose.connect(MONGODB_URI);
+  
+  // Start the API server
+  app.listen(PORT, function() {
+    console.log(`API Server now listening on PORT ${PORT}!`);
+  });
+  
